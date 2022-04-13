e50d56da87eaec775a031c84fadfc45d608701f5
Kush Patel <khpatel180@gmail.com>
2022-04-13 13:49:45 -0500

    update package

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+const Room = require('./modules/Room');
 const Timetable = require('./modules/Timetable');
 
-timetable = new Timetable('NEW', null);
+let timetable = new Timetable('NEW', null);
+
+let r1 = new Room(1, 201, 10);
+
+console.log(r1.roomStr());
